d3b54d1b11f26dfab3ef2ca6c53067f06d348b19
mathieuelisa <mathieu.elisa@gmail.com>
2021-12-09 13:28:20 +0100

    Starting the configuration of getStream

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,42 @@
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+
+const client = StreamChat.getInstance(process.env.REACT_APP_API_KEY);
+
+const channel = client.channel("aming", {
+  name: "Create a Messaging for gamers",
+  members: ["dave-matthews", "trey-anastasio"],
+});
+
+client.connect(
+  {
+    id: "dave-matthews",
+    name: "Dave Matthews",
+  },
+  process.env.REACT_APP_USER_TOKEN
+);
+
 function App() {
   return (
-    <div className="App">
-      <h1>Fluid messenger application</h1>
+    <div className="app__container">
+      <Chat client={client}>
+        <Channel channel={channel}>
+          <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput />
+          </Window>
+          <Thread />
+        </Channel>
+      </Chat>
     </div>
   );
 }
