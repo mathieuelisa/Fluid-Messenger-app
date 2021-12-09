316572afc1655c9eec8e79c6a30f1f588cb30424
mathieuelisa <mathieu.elisa@gmail.com>
2021-12-09 14:08:20 +0100

    Adding connection random user and display

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,69 @@
+import { useEffect, useState } from "react";
 import { StreamChat } from "stream-chat";
 import {
   Chat,
   Channel,
   ChannelHeader,
+  ChannelList,
   MessageInput,
   MessageList,
   Thread,
   Window,
 } from "stream-chat-react";
+import "stream-chat-css/dist/css/index.css";
+import "./app.css";
+
+const filters = { type: "messaging" };
+const options = { state: true, presence: true, limit: 10 };
+const sort = { last_message_at: -1 };
 
 const client = StreamChat.getInstance(process.env.REACT_APP_API_KEY);
 
-const channel = client.channel("aming", {
-  name: "Create a Messaging for gamers",
-  members: ["dave-matthews", "trey-anastasio"],
-});
-
-client.connect(
-  {
-    id: "dave-matthews",
-    name: "Dave Matthews",
-  },
-  process.env.REACT_APP_USER_TOKEN
-);
-
 function App() {
+  const [clientReady, setClientReady] = useState(false);
+  const [channel, setChannel] = useState(null);
+
+  useEffect(() => {
+    // Users
+    const setupClient = async () => {
+      try {
+        await client.connectUser(
+          {
+            id: "dave-matthews",
+            name: "Dave Matthews",
+          },
+          process.env.REACT_APP_USER_TOKEN
+        );
+
+        // Channel
+        const channel = await client.channel("gaming", "gaming-chat", {
+          name: "Fluid messaging for privates messages",
+        });
+        setChannel(channel);
+
+        setClientReady(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    setupClient();
+  }, []);
+
+  if (!clientReady) return null;
+
   return (
-    <div className="app__container">
-      <Chat client={client}>
-        <Channel channel={channel}>
-          <Window>
-            <ChannelHeader />
-            <MessageList />
-            <MessageInput />
-          </Window>
-          <Thread />
-        </Channel>
-      </Chat>
-    </div>
+    <Chat client={client} darkMode={true}>
+      <ChannelList filters={filters} sort={sort} options={options} />
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
   );
 }
 
