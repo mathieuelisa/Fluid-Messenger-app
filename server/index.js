c20a732595df8798786cf59a8bd7bc6602af1a6a
mathieuelisa <mathieu.elisa@gmail.com>
2021-12-11 14:26:34 +0100

    Adding bcrypt for hashing password and uuid package

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
+const PORT = 8000;
 const express = require("express");
+const bcrypt = require("bcrypt");
+const { v1: uuidv1 } = require("uuid");
+const { connect } = require("getstream");
 const app = express();
 const cors = require("cors");
-const PORT = 8000;
+
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
@@ -9,12 +14,25 @@ app.use(express.json());
 app.post("/signup", async (req, res) => {
   try {
     const { username, password } = req.body;
-    console.log(username, password);
-  } catch (error) {
-    res.json(error);
+
+    const userId = uuidv1();
+    const hashPassword = await bcrypt.hash(password, 10);
+    const client = connect(
+      process.env.API_KEY,
+      process.env.API_SECRET,
+      process.env.API_ID
+    );
+    const myTokenId = client.createUserToken(userId);
+
+    res
+      .status(200)
+      .json({ username, password, userId, hashPassword, myTokenId });
+  } catch (err) {
+    res.status(500).json({ message: err });
   }
 });
 
+//server running on 8000 port
 app.listen(PORT, () => {
   console.log(`Your server runnong on PORT: ${PORT}`);
 });
