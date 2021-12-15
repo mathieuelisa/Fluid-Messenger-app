00e6cafe3d664ce9fb279b952bb1697ffa2a516a
mathieuelisa <mathieu.elisa@gmail.com>
2021-12-15 21:47:19 +0100

    Adding hide and show users connected

--- client/src/Components/ListingofUsers/ListingOfUsers.js
+++ client/src/Components/ListingofUsers/ListingOfUsers.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+import { GiAndroidMask } from "react-icons/gi";
+
 const ListingOfUsers = ({ users }) => {
+  const [userVisibility, setUserVisibility] = useState(true);
+
+  const handleVisibility = () => {
+    console.log("clicked");
+    setUserVisibility((userVisibility) => !userVisibility);
+  };
   return (
     <div>
-      {users?.map((user) => (
-        <p key={user.id}>{user.name}</p>
-      ))}
+      <h3 onClick={handleVisibility} className="user-title">
+        Users connected and online
+      </h3>
+      {userVisibility &&
+        users?.map((user) => (
+          <div className="users-wrapper">
+            <GiAndroidMask />
+            <p key={user.id} className="users">
+              {user.name}
+            </p>
+          </div>
+        ))}
     </div>
   );
 };
